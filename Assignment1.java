b6ef91a2e9b146e1396231a133eef64dc965bca0
suchita12900 <22BCS12900@cuchd.in>
2025-03-17 12:49:15 +0530

    Add files via upload

---
+++ Assignment1.java
@@ -0,0 +1,54 @@
+/*
+public class Assignment1{
+    public static void main(String[]args)
+    {
+        System.out.println("Hello Students");
+    }
+}
+
+
+
+public class Assignment1{
+    public static void main(String[]args)
+    {
+        System.out.println("Hello\nStudents");
+    }
+}
+
+
+public class Assignment1{
+    public static void main(String[]args)
+    {
+        System.out.println("\"MySirG\"");
+    }
+}
+
+
+
+
+public class Assignment1{
+    public static void main(String[]args)
+    {
+        System.out.println("\"Teacher's Day\"");
+    }
+}
+
+
+public class Assignment1 {
+    public static void main(String[]args)
+    {
+        int a=4;
+        System.out.println("value of x is "+a);
+    }
+}
+
+ */
+
+public class Assignment1 {
+    public static void main(String[]args)
+    {
+        float a=4;
+        System.out.println("x="+a);
+
+    }
+}
